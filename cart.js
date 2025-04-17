cf874c29789b7d3f9ed7a148b6c0eed3c146ae66
Vyacheslav-Chop <syavchik260119901@gmail.com>
2025-04-17 15:31:38 +0000

    Deploying to gh-pages from @ Vyacheslav-Chop/blended-product-less7@e48ef45143aee45457f75dca48f8fc911396fee8 🚀

--- cart.js
+++ cart.js
@@ -1,2 +1,2 @@
-import{u as a,e as o,f as s,o as e,a as n,S as c}from"./assets/modal-k2592HCH.js";import"./assets/vendor-CG2pNq4b.js";const{cartProducts:r,cartBtnModal:d,cartItems:i,cartSpan:m,cartTotal:p,wishlistSpan:u}=n,{cart:t,wishlist:l}=c;a(m,t);a(i,t);o(p,t);a(u,l);d.textContent="Remove from Cart";s(r,t);r.addEventListener("click",e);
+import{u as a,e as o,f as s,o as e,a as n,S as c}from"./assets/modal-BZq91Tdd.js";import"./assets/vendor-CG2pNq4b.js";const{cartProducts:r,cartBtnModal:d,cartItems:i,cartSpan:m,cartTotal:p,wishlistSpan:u}=n,{cart:t,wishlist:l}=c;a(m,t);a(i,t);o(p,t);a(u,l);d.textContent="Remove from Cart";s(r,t);r.addEventListener("click",e);
 //# sourceMappingURL=cart.js.map
--- wishlist.js
+++ wishlist.js
@@ -1,2 +1,2 @@
-import{u as t,r as s,o as i,a,S as o}from"./assets/modal-k2592HCH.js";import"./assets/vendor-CG2pNq4b.js";const{cartSpan:r,wishlistBtnModal:n,wishlistProducts:e,wishlistSpan:c}=a,{cart:d,wishlist:l}=o;t(r,d);t(c,l);n.textContent="Remove from Wishlist";s();e.addEventListener("click",i);
+import{u as t,r as s,o as i,a,S as o}from"./assets/modal-BZq91Tdd.js";import"./assets/vendor-CG2pNq4b.js";const{cartSpan:r,wishlistBtnModal:n,wishlistProducts:e,wishlistSpan:c}=a,{cart:d,wishlist:l}=o;t(r,d);t(c,l);n.textContent="Remove from Wishlist";s();e.addEventListener("click",i);
 //# sourceMappingURL=wishlist.js.map
