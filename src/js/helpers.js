b94b31ed76e9000510dd4b036683ee40eeec21f6
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-04-21 21:27:41 +0300

    add btn go-top: index.html, go-top.css, helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -148,3 +148,26 @@ export function toggleNotFoundVisibility(isVisible) {
     notFoundDiv.classList.remove('not-found--visible');
   }
 }
+
+const goTopBtn = document.querySelector(".go-top");
+
+goTopBtn.addEventListener("click", goTop);
+window.addEventListener("scroll", trackScroll);
+
+function trackScroll() {
+  const Offset = window.pageYOffset;
+  // вичисляєм висоту вікна браузера
+  const coords = document.documentElement.clientHeight;  
+  if (Offset > coords) {
+    goTopBtn.classList.add("go-top--show");
+  } else {
+    goTopBtn.classList.remove("go-top--show");
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -75);
+    setTimeout(goTop, 0);
+  }
+}
