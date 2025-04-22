173ae34307e8d3caf08afa5b968cfc83091f2013
Vyacheslav-Chop <syavchik260119901@gmail.com>
2025-04-22 10:37:53 +0000

    Deploying to gh-pages from @ Vyacheslav-Chop/blended-product-less7@16ded520db43f24536f29ac915bfad9b7bc7dd06 🚀

--- wishlist.js
+++ wishlist.js
@@ -1,2 +1,2 @@
-import{a as t,u as s,r as a,o as r,g as o,t as e,b as i,S as n}from"./assets/theme-BZZojwTp.js";import"./assets/vendor-CG2pNq4b.js";const{cartSpan:c,wishlistProducts:d,wishlistSpan:l,goTopBtn:p}=i,{cart:S,wishlist:h}=n;t();s(c,S);s(l,h);a();d.addEventListener("click",r);p.addEventListener("click",o);window.addEventListener("scroll",e);
+import{a as t,u as s,r as a,o as r,g as o,t as e,b as i,S as n}from"./assets/theme-CxzVhMPW.js";import"./assets/vendor-CG2pNq4b.js";const{cartSpan:c,wishlistProducts:d,wishlistSpan:l,goTopBtn:p}=i,{cart:S,wishlist:h}=n;t();s(c,S);s(l,h);a();d.addEventListener("click",r);p.addEventListener("click",o);window.addEventListener("scroll",e);
 //# sourceMappingURL=wishlist.js.map
